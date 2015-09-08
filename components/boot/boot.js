d1817a13a89061e35d224b23ff9505733704e429
wp <myseawp@163.com>
2015-09-08 17:12:38 +0800

    init

---
+++ components/boot/boot.js
@@ -0,0 +1,7 @@
+'use strict';
+
+require('../dependcss/dependcss');
+
+module.exports = function () {
+    document.body.innerHTML = '<h1>Hello world!</h1>';
+};
---
+++ components/dependcss/dependcss.js
@@ -0,0 +1,8 @@
+/**
+ * Created by WP on 2015/9/8.
+ */
+'use strict';
+
+module.exports = function () {
+
+};
